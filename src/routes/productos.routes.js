a75839e315ebbd4a0a1155b26681b8fdfee52979
gdiazi <guillermo.diazipa@gmail.com>
2023-04-26 23:43:05 -0500

    añadiendo el CRUD de productos

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,12 +1,38 @@
 import { Router } from "express";
 import * as controllers from "../controllers/productos.controllers.js";
-import { validarToken, esAdmin } from "../utils/wachiman.js";
+import { listarProductos } from "../controllers/productos.controllers.js";
+import { DevolverProducto } from "../controllers/productos.controllers.js";
+import { actualizarProducto } from "../controllers/productos.controllers.js";
+import { eliminarProducto } from "../controllers/productos.controllers.js";
+//import { validarToken, esAdmin } from "../utils/wachiman.js";
 
 export const productoRouter = Router();
 
 productoRouter.post(
   "/productos",
-  validarToken,
-  esAdmin,
   controllers.crearProducto
 );
+
+
+productoRouter.get(
+  "/productos", 
+  listarProductos
+  );
+
+  productoRouter.get(
+    "/producto/:id", 
+    DevolverProducto
+  );  
+
+
+  productoRouter.put(
+    "/producto/:id", 
+    actualizarProducto
+    );
+
+
+    productoRouter.delete(
+      "/producto/:id",
+      eliminarProducto
+      );
+    
